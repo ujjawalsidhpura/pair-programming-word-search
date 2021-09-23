4ad11d76b067485141070c7a5c6eca3c8cf6425b
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-09-23 00:01:36 +0000

    func refactored and tested

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -3,8 +3,8 @@ const assert = chai.assert;
 
 const wordSearch = require('../wordsearch.js')
 
-describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+describe("#wordSearch()", function () {
+  it("should return false if the word is not present NEITHER VERTICALLY NOR HORIZONTALLY", function () {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present HORIZONTALLY", function () {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -32,6 +32,22 @@ describe("#wordSearch()", function() {
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present VERTICALLY", function () {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'WEFMHFBDZ')
 
     assert.isTrue(result);
   });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,35 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
+    //To get all horizontal words in an array 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+
+    //to get all vertical words in an array
+    let verticalJoin = [];
+    let temp = '';
+    for (let i = 0; i < letters[0].length; i++) {
+        letters.map((arr) => {
+            temp += arr[i];
+        })
+        verticalJoin.push(temp);
+        temp = ''
     }
+
+    //Looping over to check either horizontal or vertical array contains the word. 
+
+    for (let i = 0; i < horizontalJoin.length; i++) {
+        if (horizontalJoin[i] === word) {
+            return true
+        }
+    }
+
+    for (let i = 0; i < verticalJoin.length; i++) {
+        if (verticalJoin[i] === word) {
+            return true
+        }
+    }
+
+    //If none horizontal nor vertical contains the words,then return false
+    return false;
+
 }
 
 module.exports = wordSearch
